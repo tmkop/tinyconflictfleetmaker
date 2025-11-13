c54c17a9d0846aa8fb82b2945a54972a430035b3
tmkop <tmkop2005@gmail.com>
2025-11-13 18:34:49 -0500

    add template for hide/show faction builder page

--- scripts.js
+++ scripts.js
@@ -1,13 +1,15 @@
+const main = document.getElementById('main');
 document.addEventListener('DOMContentLoaded', () => {
     addPage('title');
+    addPage('faction-builder');
     addPage('ships');
+    updateFaction();
 
 });
 
 function addPage(templateName) {
     const tpl = document.getElementById('page-template');
 
-    const main = document.getElementById('main');
 
     // Extract the page element from the fragment so we can attach handlers
     const pageElement = tpl.content.cloneNode(true).querySelector('.page');
@@ -22,8 +24,20 @@ function addPage(templateName) {
 
 }
 
-
-
 function loadTemplate(container,template){
     container.innerHTML = template.innerHTML;
+}
+
+function updateFaction(){
+    const faction = document.getElementById("faction-selector").value;
+
+    // custom factions have their own faction-building page
+    if(faction === "custom" && !main.classList.contains("custom-faction")){
+        main.classList.add("custom-faction");
+    }else{
+        main.classList.remove("custom-faction");
+    }
+    //need to check if any faction-specific stuff will be removed and alert about it
+
+
 }
