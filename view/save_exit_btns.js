ad17b270fe7af0cb84659088a5fded21a4d6c3c6
tmkop <tmkop2005@gmail.com>
2026-02-21 12:34:17 -0500

    Create save_exit_btns.js

---
+++ view/save_exit_btns.js
@@ -0,0 +1,11 @@
+document.getElementById("save-btn").addEventListener("click",(e)=>{
+    alert("Needs Implementation");
+});
+
+document.getElementById("exit-btn").addEventListener("click",(e)=>{
+    window.location.href = "/list";
+});
+
+document.getElementById("save-exit-btn").addEventListener("click",(e)=>{
+    window.location.href = "/list";
+});
